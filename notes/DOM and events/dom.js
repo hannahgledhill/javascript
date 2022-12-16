332b213fc180b445e61f33f3780f1bb85042ec74
Hannah Gledhill <hannah.gledhill@etempa.co.uk>
2022-12-16 18:06:13 +0000

    started on DOM stuff

---
+++ notes/DOM and events/dom.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Document Object Model - a tree-like structure of objects where each html element is an object with properties and methods
+ * Each object in the tree is known as a node, and it represents a html element on the page
+ * 
+ * The DOCUMENT object is at the top of the tree - it's like the big parent object holding all the child nodes. We call it the "entry point" to the DOM
+ * 
+ * text, alt tags, comments and other things are actually separate nodes within element nodes sometimes
+ * 
+ * DOM and DOM methods are part of the Web APIs which browsers implement and which we can access using javascript code. They are basically libraries available in all browsers for us to use.
+ * 
+ * There are other Web APIs apart from DOM such as timers, fetch etc.
+ * 
+ */
